62274b05b4faf38f36c2d693618c7e4e90231a5b
Guo Yunhe <i@guoyunhe.me>
2026-01-04 20:15:28 +0800

    feat: update image model to support type parameter in fromFs and fromBuffer methods

--- app/models/image.ts
+++ app/models/image.ts
@@ -6,27 +6,28 @@ import { readFile } from 'node:fs/promises';
 import sharp from 'sharp';
 
 export default class Image extends BaseModel {
-  static storage = 'images';
-
-  static async fromFs(path: string): Promise<Image> {
+  static async fromFs(path: string, type?: string): Promise<Image> {
     const buffer = await readFile(path);
-    return this.fromBuffer(buffer);
+    return this.fromBuffer(buffer, type);
   }
 
-  static async fromBuffer(buffer: Buffer): Promise<Image> {
+  static async fromBuffer(buffer: Buffer, type?: string): Promise<Image> {
     const hash = createHash('md5');
     const md5 = hash.update(buffer).digest('hex');
 
     const { width, height, format, size } = await sharp(buffer).metadata();
 
     const disk = drive.use();
-    const image = await Image.firstOrCreate({
-      width,
-      height,
-      format,
-      size,
-      md5,
-    });
+    const image = await Image.firstOrCreate(
+      {
+        width,
+        height,
+        format,
+        size,
+        md5,
+      },
+      { type },
+    );
 
     if (!(await disk.exists(image.path))) {
       await disk.put(image.path, buffer);
@@ -50,8 +51,14 @@ export default class Image extends BaseModel {
   @column()
   declare size: number;
 
+  @column({
+    // Otherwise AdonisJS mistakes column name to md_5
+    columnName: 'md5',
+  })
+  declare md5: string | null;
+
   @column()
-  declare md5: string;
+  declare type: string | null;
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime;
@@ -60,7 +67,7 @@ export default class Image extends BaseModel {
   declare updatedAt: DateTime;
 
   get path() {
-    return `${Image.storage}/${this.md5}.${this.format}`;
+    return `images/${this.md5}.${this.format}`;
   }
 
   @computed()
