3e5fa2c9a0ffd68ea4d0b4a7286b59258e547924
Guo Yunhe <i@guoyunhe.me>
2026-01-15 00:03:40 +0800

    feat(user): 添加头像关系，更新用户模型以支持用户头像

--- app/models/user.ts
+++ app/models/user.ts
@@ -2,8 +2,10 @@ import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens';
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid';
 import { compose } from '@adonisjs/core/helpers';
 import hash from '@adonisjs/core/services/hash';
-import { BaseModel, column } from '@adonisjs/lucid/orm';
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm';
+import type { BelongsTo } from '@adonisjs/lucid/types/relations';
 import { DateTime } from 'luxon';
+import Image from './image.js';
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email', 'username'],
@@ -36,4 +38,9 @@ export default class User extends compose(BaseModel, AuthFinder) {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null;
+
+  // Relationships
+
+  @belongsTo(() => Image, { foreignKey: 'avatarId' })
+  declare avatar: BelongsTo<typeof Image>;
 }
