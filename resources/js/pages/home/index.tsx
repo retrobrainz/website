8b1267c44e29209be5df0beb4208ccf24c0a19b8
Guo Yunhe <i@guoyunhe.me>
2026-01-16 23:45:15 +0800

    feat(home): 替换平台列表展示为卡片布局，移除表格组件

--- resources/js/pages/home/index.tsx
+++ resources/js/pages/home/index.tsx
@@ -1,42 +1,36 @@
-import { Table } from 'antd';
+import { Card, Col, Row } from 'antd';
+import { Container } from 'antd-moe';
 import { useFetch } from 'react-fast-fetch';
+import { Link } from 'wouter';
 
 export default function HomePage() {
   const { data } = useFetch<any[]>('/platforms');
 
   return (
-    <div>
-      <Table
-        tableLayout="fixed"
-        dataSource={data}
-        pagination={false}
-        columns={[
-          {
-            dataIndex: ['company', 'name'],
-            title: 'Company',
-            sorter: (a: any, b: any) => a.company.name.localeCompare(b.company.name),
-          },
-          {
-            dataIndex: 'name',
-            title: 'Name',
-            render: (name: string, { id }: any) => <a href={`/platforms/${id}`}>{name}</a>,
-            sorter: (a: any, b: any) => a.company.name.localeCompare(b.company.name),
-          },
-          {
-            dataIndex: 'releaseDate',
-            title: 'Release Date',
-            render: (date: string) => new Date(date).toLocaleDateString(),
-            sorter: (a: any, b: any) =>
-              new Date(a.releaseDate).getTime() - new Date(b.releaseDate).getTime(),
-          },
-          {
-            dataIndex: 'gamesCount',
-            title: 'Games Count',
-            defaultSortOrder: 'descend',
-            sorter: (a: any, b: any) => a.gamesCount - b.gamesCount,
-          },
-        ]}
-      />
-    </div>
+    <Container>
+      <Row gutter={[16, 16]} style={{ marginTop: 16 }}>
+        {data
+          ?.sort((a, b) => b.gamesCount - a.gamesCount)
+          ?.map((platform) => (
+            <Col key={platform.id} xs={24} sm={12} md={8} lg={6} xl={4}>
+              <Link href={`/platforms/${platform.id}`}>
+                <Card
+                  cover={
+                    <img
+                      alt={platform.name}
+                      src={`https://github.com/libretro/retroarch-assets/blob/master/xmb/systematic/png/${encodeURIComponent(`${platform.company.name} - ${platform.name}`)}.png?raw=true`}
+                      width={256}
+                      height={256}
+                      style={{ height: 'auto', background: '#bbb' }}
+                    />
+                  }
+                >
+                  <Card.Meta title={platform.name} description={platform.company.name} />
+                </Card>
+              </Link>
+            </Col>
+          ))}
+      </Row>
+    </Container>
   );
 }
