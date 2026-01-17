deda6d7708a73ba9ad3e8f912e1774faff117746
Guo Yunhe <i@guoyunhe.me>
2026-01-17 16:36:21 +0800

    fix(GameImagesController): switch to GameImage model for image storage

--- app/controllers/game_images_controller.ts
+++ app/controllers/game_images_controller.ts
@@ -1,15 +1,16 @@
-import Game from '#models/game';
+import GameImage from '#models/game_image';
 import type { HttpContext } from '@adonisjs/core/http';
 
 export default class GameImagesController {
   async store({ params, request }: HttpContext) {
     // TODO: validate input
     const { game_id } = params;
-    const game = await Game.findOrFail(game_id);
-    return await game.related('images').firstOrCreate({
-      gameId: game_id,
-      imageId: request.input('imageId'),
-      type: request.input('type'),
-    });
+    return await GameImage.firstOrCreate(
+      {
+        gameId: game_id,
+        imageId: request.input('imageId'),
+      },
+      { type: request.input('type') },
+    );
   }
 }
