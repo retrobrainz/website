7ffcdbed628e852432dbc67976fe611bea148e42
Guo Yunhe <i@guoyunhe.me>
2026-01-18 20:31:32 +0800

    feat(game): enhance game page layout with Descriptions and Typography components

--- resources/js/pages/game/index.tsx
+++ resources/js/pages/game/index.tsx
@@ -1,14 +1,13 @@
-import { Breadcrumb } from 'antd';
+import { Breadcrumb, Descriptions, Typography } from 'antd';
 import { Container } from 'antd-moe';
 import { useFetch } from 'react-fast-fetch';
 import { Link, useParams } from 'wouter';
+import Game from '../../types/Game.js';
 
 export default function GamePage() {
-  const { gameId, platformId } = useParams();
+  const { gameId } = useParams();
 
-  const { data: platform } = useFetch<any>(`/platforms/${platformId}`);
-
-  const { data: game } = useFetch<any>(`/games/${gameId}`);
+  const { data: game } = useFetch<Game>(`/games/${gameId}`);
 
   return (
     <Container maxWidth="lg" style={{ paddingTop: 16 }}>
@@ -16,7 +15,9 @@ export default function GamePage() {
         items={[
           { title: <Link href="/">Home</Link> },
           {
-            title: <Link href={`/platforms/${platformId}`}>{platform?.name || '...'}</Link>,
+            title: (
+              <Link href={`/platforms/${game?.platform?.id}`}>{game?.platform?.name || '...'}</Link>
+            ),
           },
           {
             title: game?.name || '...',
@@ -25,7 +26,32 @@ export default function GamePage() {
         style={{ marginBottom: 16 }}
       />
 
-      <h1>{game?.name}</h1>
+      <Typography.Title level={1}>{game?.name}</Typography.Title>
+
+      <Descriptions
+        items={[
+          {
+            label: 'Name',
+            children: game?.name || '...',
+          },
+          {
+            label: 'Region(s)',
+            children: game?.regions?.map((region) => region.name).join(', ') || 'N/A',
+          },
+          {
+            label: 'Release Date',
+            children: game?.releaseDate || 'N/A',
+          },
+          {
+            label: 'ESRB Rating',
+            children: game?.esrbRating || 'N/A',
+          },
+          {
+            label: 'PEGI Rating',
+            children: game?.pegiRating || 'N/A',
+          },
+        ]}
+      />
 
       <p>Details about the game will go here.</p>
     </Container>
