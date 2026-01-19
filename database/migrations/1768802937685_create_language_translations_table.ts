7e24202d86c89b852cb0759552faa58a5c5c173d
Guo Yunhe <i@guoyunhe.me>
2026-01-19 19:35:12 +0800

    feat: create language translations table with relationships and constraints

--- database/migrations/1768802937685_create_language_translations_table.ts
+++ database/migrations/1768802937685_create_language_translations_table.ts
@@ -1,28 +1,28 @@
 import { BaseSchema } from '@adonisjs/lucid/schema';
 
 export default class extends BaseSchema {
-  protected tableName = 'region_translations';
+  protected tableName = 'language_translations';
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id');
 
       table
-        .integer('region_id')
+        .integer('language_id')
         .notNullable()
         .unsigned()
         .references('id')
-        .inTable('regions')
+        .inTable('languages')
         .onDelete('CASCADE');
 
       table.string('locale', 5).notNullable();
 
-      table.string('name', 64).notNullable();
+      table.string('name', 32).notNullable();
 
       table.timestamp('created_at');
       table.timestamp('updated_at');
 
-      table.unique(['region_id', 'locale']);
+      table.unique(['language_id', 'locale']);
     });
   }
 
