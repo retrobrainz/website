22437d487633559764e0e1a3b6e73e6770decdbd
Guo Yunhe <i@guoyunhe.me>
2026-01-21 22:13:25 +0800

    feat(i18n): add i18n support with DetectUserLocaleMiddleware and configuration

--- adonisrc.ts
+++ adonisrc.ts
@@ -50,6 +50,7 @@ export default defineConfig({
     () => import('@adonisjs/lucid/database_provider'),
     () => import('@adonisjs/auth/auth_provider'),
     () => import('@adonisjs/drive/drive_provider'),
+    () => import('@adonisjs/i18n/i18n_provider'),
   ],
 
   /*
@@ -94,6 +95,10 @@ export default defineConfig({
     },
     {
       pattern: 'public/**',
+      reloadServer: false,
+    },
+    {
+      pattern: 'resources/lang/**/*.{json,yaml,yml}',
       reloadServer: false,
     },
   ],
