226dd077eae81bdf33aaf9c992ebf7584b369829
Guo Yunhe <i@guoyunhe.me>
2026-01-21 22:16:31 +0800

    feat(i18n): update supported locales and clean up comments in i18n configuration

--- config/i18n.ts
+++ config/i18n.ts
@@ -3,18 +3,10 @@ import { defineConfig, formatters, loaders } from '@adonisjs/i18n';
 
 const i18nConfig = defineConfig({
   defaultLocale: 'en',
+  supportedLocales: ['en', 'fr', 'es', 'de', 'it', 'ja', 'zh'],
   formatter: formatters.icu(),
 
   loaders: [
-    /**
-     * The fs loader will read translations from the
-     * "resources/lang" directory.
-     *
-     * Each subdirectory represents a locale. For example:
-     *   - "resources/lang/en"
-     *   - "resources/lang/fr"
-     *   - "resources/lang/it"
-     */
     loaders.fs({
       location: app.languageFilesPath(),
     }),
