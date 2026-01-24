dece58e606b081d9acf3beb7c5192b1171d678ee
Guo Yunhe <i@guoyunhe.me>
2026-01-24 18:32:11 +0800

    feat: add icon support to emulators in seeder

--- database/seeders/3_emulators_seeder.ts
+++ database/seeders/3_emulators_seeder.ts
@@ -1,4 +1,5 @@
 import Emulator from '#models/emulator';
+import Image from '#models/image';
 import Platform from '#models/platform';
 import { BaseSeeder } from '@adonisjs/lucid/seeders';
 import { DateTime } from 'luxon';
@@ -12,6 +13,7 @@ export default class extends BaseSeeder {
         website: 'https://www.retroarch.com',
         state: 'stable',
         releaseDate: DateTime.fromISO('2010-05-23'),
+        icon: 'https://github.com/libretro/RetroArch/blob/master/media/retroarch-vector_invader-only.svg?raw=true',
         platforms: [
           'Nintendo Entertainment System',
           'Family Computer Disk System',
@@ -40,6 +42,7 @@ export default class extends BaseSeeder {
       {
         name: 'PCSX2',
         website: 'https://pcsx2.net',
+        icon: 'https://github.com/PCSX2/pcsx2/blob/master/bin/resources/icons/AppIconLarge.png?raw=true',
         state: 'stable',
         releaseDate: DateTime.fromISO('2002-09-22'),
         platforms: ['PlayStation 2'],
@@ -47,23 +50,18 @@ export default class extends BaseSeeder {
       {
         name: 'RPCS3',
         website: 'https://rpcs3.net',
+        icon: 'https://github.com/RPCS3/rpcs3/blob/master/rpcs3/rpcs3.svg?raw=true',
         state: 'stable',
         releaseDate: DateTime.fromISO('2011-05-23'),
         platforms: ['PlayStation 3'],
       },
       {
         name: 'Eden',
-        website: null,
-        state: 'experimental',
-        releaseDate: null,
+        website: 'https://eden-emu.dev/',
+        icon: 'https://git.eden-emu.dev/eden-emu/eden/raw/branch/master/dist/eden.bmp',
+        state: 'stable',
+        releaseDate: DateTime.fromISO('2025-05-10'),
         platforms: ['Switch'],
-      },
-      {
-        name: 'Citra',
-        website: null,
-        state: 'discontinued',
-        releaseDate: DateTime.fromISO('2015-10-04'),
-        platforms: ['Nintendo 3DS'],
       },
       {
         name: 'Dolphin',
@@ -82,8 +80,14 @@ export default class extends BaseSeeder {
     ];
 
     // Create emulators and link them to platforms
-    for (const { name, platforms: platformNames, ...emulatorData } of emulators) {
-      const emulator = await Emulator.firstOrCreate({ name }, { name, ...emulatorData });
+    for (const { name, icon: iconUrl, platforms: platformNames, ...emulatorData } of emulators) {
+      const icon = iconUrl
+        ? await Image.fromHttp(iconUrl, { width: 256, height: 256, format: 'avif' })
+        : null;
+      const emulator = await Emulator.firstOrCreate(
+        { name },
+        { name, iconId: icon?.id ?? null, ...emulatorData },
+      );
 
       if (platformNames.length > 0) {
         const platforms = await Platform.query().whereIn('name', platformNames);
