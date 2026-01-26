6c1699790a744055293712170a0c207f6225d7b8
Guo Yunhe <i@guoyunhe.me>
2026-01-26 23:11:53 +0800

    feat: implement caching for image downloads in platform seeder

--- database/seeders/0_platforms_seeder.ts
+++ database/seeders/0_platforms_seeder.ts
@@ -1,9 +1,11 @@
 import Company from '#models/company';
 import Image from '#models/image';
 import Platform from '#models/platform';
+import app from '@adonisjs/core/services/app';
 import { BaseSeeder } from '@adonisjs/lucid/seeders';
-import { sleep } from '@guoyunhe/sleep';
+import { download } from '@guoyunhe/downloader';
 import { DateTime } from 'luxon';
+import { existsSync } from 'node:fs';
 
 export default class extends BaseSeeder {
   /**
@@ -12,6 +14,13 @@ export default class extends BaseSeeder {
    * @see https://en.wikipedia.org/wiki/List_of_best-selling_game_consoles
    */
   async run() {
+    if (!existsSync(app.tmpPath('seed-images-main'))) {
+      await download(
+        'https://github.com/retrobrainz/seed-images/archive/refs/heads/main.tar.gz',
+        app.tmpPath(),
+        { extract: true },
+      );
+    }
     // Nintendo consoles
     const nintendo = await Company.firstOrCreate({ name: 'Nintendo' });
     for (const props of [
@@ -20,128 +29,84 @@ export default class extends BaseSeeder {
         screenWidth: 256,
         screenHeight: 240,
         releaseDate: DateTime.fromISO('1983-07-15'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/0d/NES_logo.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/NES-Console-Set.png/1280px-NES-Console-Set.png',
       },
       {
         name: 'Family Computer Disk System',
         screenWidth: 256,
         screenHeight: 240,
         releaseDate: DateTime.fromISO('1986-02-21'),
-        logoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/c/c7/Family_Computer_Disk_System_logo.png',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Nintendo-Famicom-Disk-System.png/1280px-Nintendo-Famicom-Disk-System.png',
       },
       {
         name: 'Game Boy',
         screenWidth: 160,
         screenHeight: 144,
         releaseDate: DateTime.fromISO('1989-04-21'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/f/f2/Nintendo_Game_Boy_Logo.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Game-Boy-FL.png/1280px-Game-Boy-FL.png',
       },
       {
         name: 'Super Nintendo Entertainment System',
         screenWidth: 256,
         screenHeight: 224,
         releaseDate: DateTime.fromISO('1990-11-21'),
-        logoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/3/33/Super_Nintendo_Entertainment_System_logo.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Nintendo-Super-Famicom-Set-FL.png/1280px-Nintendo-Super-Famicom-Set-FL.png',
       },
       {
         name: 'Nintendo 64',
         screenWidth: 320,
         screenHeight: 240,
         releaseDate: DateTime.fromISO('1996-06-23'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/en/2/2d/Nintendo_64_%28logo%29.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/N64-Console-Set.png/1280px-N64-Console-Set.png',
       },
       {
         name: 'Game Boy Color',
         screenWidth: 160,
         screenHeight: 144,
         releaseDate: DateTime.fromISO('1998-10-21'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/c/c5/Game_Boy_Color_logo.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Nintendo-Game-Boy-Color-FL.png/1280px-Nintendo-Game-Boy-Color-FL.png',
       },
       {
         name: 'Game Boy Advance',
         screenWidth: 240,
         screenHeight: 160,
         releaseDate: DateTime.fromISO('2001-03-21'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/50/Game_Boy_Advance_logo.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Nintendo-Game-Boy-Advance-Purple-FL.png/1280px-Nintendo-Game-Boy-Advance-Purple-FL.png',
       },
       {
         name: 'GameCube',
         screenWidth: 640,
         screenHeight: 480,
         releaseDate: DateTime.fromISO('2001-09-14'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/en/e/e6/Nintendo_Gamecube_Logo.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/GameCube-Console-Set.png/1280px-GameCube-Console-Set.png',
       },
       {
         name: 'Nintendo DS',
         screenWidth: 256,
         screenHeight: 384,
         releaseDate: DateTime.fromISO('2004-11-21'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/af/Nintendo_DS_Logo.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Nintendo-DS-Lite-Black-Open.png/1280px-Nintendo-DS-Lite-Black-Open.png',
       },
       {
         name: 'Wii',
         screenWidth: 640,
         screenHeight: 480,
         releaseDate: DateTime.fromISO('2006-11-19'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Wii.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Wii-Console.png/1280px-Wii-Console.png',
       },
       {
         name: 'Nintendo DSi',
         screenWidth: 256,
         screenHeight: 384,
         releaseDate: DateTime.fromISO('2008-11-01'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/d/dd/Nintendo_DSi_logo.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Nintendo-DSi-Bl-Open.png/1280px-Nintendo-DSi-Bl-Open.png',
       },
       {
         name: 'Nintendo 3DS',
         screenWidth: 400,
         screenHeight: 240,
         releaseDate: DateTime.fromISO('2011-02-26'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/8/89/Nintendo_3DS_logo.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Nintendo-3DS-AquaOpen.png/1280px-Nintendo-3DS-AquaOpen.png',
       },
       {
         name: 'Wii U',
         screenWidth: 1280,
         screenHeight: 720,
         releaseDate: DateTime.fromISO('2012-11-18'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/7e/WiiU.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Wii_U_Console_and_Gamepad.png/1280px-Wii_U_Console_and_Gamepad.png',
       },
       {
         name: 'Switch',
         screenWidth: 1280,
         screenHeight: 720,
         releaseDate: DateTime.fromISO('2017-03-03'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Nintendo_Switch_logo.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Nintendo-Switch-wJoyCons-BlRd-Standing-FL.png/1280px-Nintendo-Switch-wJoyCons-BlRd-Standing-FL.png',
       },
     ]) {
       await this.createPlatform({ companyId: nintendo.id, ...props });
@@ -155,56 +120,36 @@ export default class extends BaseSeeder {
         screenWidth: 320,
         screenHeight: 240,
         releaseDate: DateTime.fromISO('1994-12-03'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/4e/Playstation_logo_colour.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/PSX-Console-wController.png/1280px-PSX-Console-wController.png',
       },
       {
         name: 'PlayStation 2',
         screenWidth: 640,
         screenHeight: 480,
         releaseDate: DateTime.fromISO('2000-03-04'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/76/PlayStation_2_logo.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Sony-PlayStation-2-30001-wController-L.png/1280px-Sony-PlayStation-2-30001-wController-L.png',
       },
       {
         name: 'PlayStation Portable',
         screenWidth: 480,
         screenHeight: 272,
         releaseDate: DateTime.fromISO('2004-12-12'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/PSP_Logo.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/PSP-1000.png/1280px-PSP-1000.png',
       },
       {
         name: 'PlayStation 3',
         screenWidth: 1280,
         screenHeight: 720,
         releaseDate: DateTime.fromISO('2006-11-11'),
-        logoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/0/05/PlayStation_3_logo_%282009%29.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Sony-PlayStation-3-4001B-wController-noBG.png/1280px-Sony-PlayStation-3-4001B-wController-noBG.png',
       },
       {
         name: 'PlayStation Vita',
         screenWidth: 960,
         screenHeight: 544,
         releaseDate: DateTime.fromISO('2011-12-17'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/3/3d/PlayStation_Vita_logo.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/PlayStation-Vita-1101-FL.png/1280px-PlayStation-Vita-1101-FL.png',
       },
       {
         name: 'PlayStation 4',
         screenWidth: 1920,
         screenHeight: 1080,
         releaseDate: DateTime.fromISO('2013-11-15'),
-        logoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/8/87/PlayStation_4_logo_and_wordmark.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/PS4-Console-wDS4.png/1280px-PS4-Console-wDS4.png',
       },
     ]) {
       await this.createPlatform({ companyId: sony.id, ...props });
@@ -218,87 +163,71 @@ export default class extends BaseSeeder {
         screenWidth: 256,
         screenHeight: 192,
         releaseDate: DateTime.fromISO('1985-10-20'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/3/3e/Sega-master-system-logo.png',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Sega-Master-System-Set.png/1280px-Sega-Master-System-Set.png',
       },
       {
         name: 'Mega Drive - Genesis',
         screenWidth: 320,
         screenHeight: 240,
         releaseDate: DateTime.fromISO('1988-10-29'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/en/1/12/GenesisLogo.png',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Sega-Genesis-Mk2-6button.png/1280px-Sega-Genesis-Mk2-6button.png',
       },
       {
         name: 'Game Gear',
         screenWidth: 160,
         screenHeight: 144,
         releaseDate: DateTime.fromISO('1990-10-06'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/b/b8/Game_Gear_logo_Sega.png',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Sega-Game-Gear-WB.png/1280px-Sega-Game-Gear-WB.png',
       },
       {
         name: 'Mega-CD - Sega CD',
         screenWidth: 320,
         screenHeight: 240,
         releaseDate: DateTime.fromISO('1991-12-17'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Sega_CD_Logo.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Sega-CD-Model2-Set.png/1280px-Sega-CD-Model2-Set.png',
       },
       {
         name: '32X',
         screenWidth: 320,
         screenHeight: 240,
         releaseDate: DateTime.fromISO('1994-11-21'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a4/Sega_32X_logo.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Sega-Genesis-Model2-32X.png/1280px-Sega-Genesis-Model2-32X.png',
       },
       {
         name: 'Saturn',
         screenWidth: 320,
         screenHeight: 240,
         releaseDate: DateTime.fromISO('1994-11-22'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Sega_Saturn_USA_logo.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Sega-Saturn-Console-Set-Mk2.png/1280px-Sega-Saturn-Console-Set-Mk2.png',
       },
       {
         name: 'Dreamcast',
         screenWidth: 640,
         screenHeight: 480,
         releaseDate: DateTime.fromISO('1999-11-27'),
-        logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/8/83/Dreamcast_logo_Japan.svg',
-        photoUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Dreamcast-Console-Set.png/1280px-Dreamcast-Console-Set.png',
       },
     ]) {
       await this.createPlatform({ companyId: sega.id, ...props });
     }
   }
 
-  async createPlatform({ companyId, name, logoUrl, photoUrl, ...rest }: any) {
+  async createPlatform({ companyId, name, ...rest }: any) {
     const platform = await Platform.firstOrCreate({ companyId, name }, rest);
 
-    if (!platform.logoId && logoUrl) {
-      const logo = await Image.fromHttp(logoUrl, {
-        width: 512,
-        height: 512,
-        format: 'avif',
-        fit: 'inside',
-        background: { r: 255, g: 255, b: 255, alpha: 0 },
-      });
-      platform.logoId = logo.id;
-
-      await sleep(1000); // To avoid hitting request limits
+    if (!platform.logoId) {
+      const logoPath = [
+        app.tmpPath('seed-images-main', 'platform-logos', `${name}.png`),
+        app.tmpPath('seed-images-main', 'platform-logos', `${name}.svg`),
+      ].find(existsSync);
+      if (logoPath) {
+        const logo = await Image.fromFs(logoPath, {
+          width: 512,
+          height: 512,
+          format: 'avif',
+          fit: 'inside',
+          background: { r: 255, g: 255, b: 255, alpha: 0 },
+        });
+        platform.logoId = logo.id;
+      }
     }
 
-    if (!platform.photoId && photoUrl) {
-      const photo = await Image.fromHttp(photoUrl, {
+    if (!platform.photoId) {
+      const photoPath = app.tmpPath('seed-images-main', 'platform-photos', `${name}.png`);
+      const photo = await Image.fromFs(photoPath, {
         width: 1280,
         height: 720,
         format: 'avif',
@@ -306,7 +235,6 @@ export default class extends BaseSeeder {
         background: { r: 255, g: 255, b: 255, alpha: 0 },
       });
       platform.photoId = photo.id;
-      await sleep(1000); // To avoid hitting request limits
     }
 
     await platform.save();
