ad938ec18f2f57a35827c5bf916859293d2a360c
Guo Yunhe <i@guoyunhe.me>
2026-02-01 11:18:45 +0800

    feat: enhance PlatformsPage with Breadcrumb and title for better navigation

--- resources/js/pages/platforms/index.tsx
+++ resources/js/pages/platforms/index.tsx
@@ -1,13 +1,27 @@
-import { Col, Row } from 'antd';
+import { Breadcrumb, Col, Flex, Row, Typography } from 'antd';
 import { Container } from 'antd-moe';
 import { useFetch } from 'react-fast-fetch';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'wouter';
 import PlatformCard from '../../components/platform-card/index.js';
 
 export default function PlatformsPage() {
+  const { t } = useTranslation();
   const { data } = useFetch<any[]>('/platforms');
 
   return (
     <Container style={{ paddingTop: 24 }}>
+      <Breadcrumb
+        items={[{ title: <Link href="/">{t('home')}</Link> }, { title: t('platforms') }]}
+        style={{ marginBottom: 16 }}
+      />
+
+      <Flex align="center" style={{ marginBottom: 16 }}>
+        <Typography.Title level={1} style={{ margin: 0 }}>
+          {t('platforms')}
+        </Typography.Title>
+      </Flex>
+
       <Row gutter={[24, 24]}>
         {data
           ?.sort((a, b) => b.gamesCount - a.gamesCount)
