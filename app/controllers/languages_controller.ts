78176b5151c262112f693c4a710e0f00d33bb3fb
Guo Yunhe <i@guoyunhe.me>
2026-02-10 22:27:57 +0800

    feat: refactor LanguagesController to streamline query handling with franchiseId filter

--- app/controllers/languages_controller.ts
+++ app/controllers/languages_controller.ts
@@ -1,20 +1,28 @@
 import Language from '#models/language';
 import type { HttpContext } from '@adonisjs/core/http';
 
-export default class RegionsController {
-  /**
-   * Display a list of resource
-   */
+export default class LanguagesController {
   async index({ request }: HttpContext) {
     const query = Language.query();
-    if (request.input('platformId')) {
+
+    const platformId = request.input('platformId');
+    const titleId = request.input('titleId');
+    const franchiseId = request.input('franchiseId');
+    if (platformId || titleId || franchiseId) {
       query.whereHas('games', (gameQuery) => {
-        gameQuery.where('platformId', request.input('platformId'));
-      });
-    }
-    if (request.input('titleId')) {
-      query.whereHas('games', (gameQuery) => {
-        gameQuery.where('titleId', request.input('titleId'));
+        if (platformId) {
+          gameQuery.where('platformId', platformId);
+        }
+        if (titleId) {
+          gameQuery.where('titleId', titleId);
+        }
+        if (franchiseId) {
+          gameQuery.whereHas('title', (titleQuery) => {
+            titleQuery.whereHas('franchises', (franchiseQuery) => {
+              franchiseQuery.where('franchises.id', franchiseId);
+            });
+          });
+        }
       });
     }
     return query.orderBy('name', 'asc').exec();
