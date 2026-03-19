2a1bdfd55c998f7e1d79fcc75999beedcded808a
Guo Yunhe <i@guoyunhe.me>
2026-03-19 20:50:16 +0800

    refactor: move deduplication and serial fixing functions to libretro.ts

--- utils/libretro.ts
+++ utils/libretro.ts
@@ -10,12 +10,33 @@ import Region from '#models/region';
 import Rom from '#models/rom';
 import Title from '#models/title';
 import { deleteGithubRepo, downloadGithubRepo } from '#utils/github';
-import { deduplicate, fixSerial } from '#utils/platform';
 import { parse as parseDat } from '@retrobrainz/dat';
 import { parse as parseName } from '@retrobrainz/name';
 import { existsSync } from 'fs';
 import { readFile } from 'fs/promises';
 import { DateTime } from 'luxon';
+
+export function deduplicate(games: any[]): any[] {
+  const gamesMap: Record<string, any> = {};
+  games.forEach((game) => {
+    const { crc } = game.$entries[0];
+    const serial = game.$entries[0].serial || game.serial;
+    const key = JSON.stringify({ crc, serial });
+    if (!gamesMap[key]) {
+      gamesMap[key] = game;
+    } else {
+      console.log(`Deduplicating game: ${game.name} (${crc}/${serial})`);
+      gamesMap[key].$entries = [...gamesMap[key].$entries, ...game.$entries];
+    }
+  });
+  return Object.values(gamesMap);
+}
+
+export function fixSerial(serial: string): string {
+  if (!serial) return serial;
+  if (serial === 'SLUS21568') return 'SLUS-21568';
+  return serial;
+}
 
 export async function importLibretro(platform: Platform): Promise<void> {
   await downloadGithubRepo('libretro', 'libretro-database');
