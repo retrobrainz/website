19bc821b8a2648d51dd535556d3de542d5a8a7ea
Guo Yunhe <i@guoyunhe.me>
2026-03-22 11:53:40 +0800

    fix: update application name to 'retrobrainz' and add memory configuration

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: 'web-app',
+      name: 'retrobrainz',
       script: './build/bin/server.js',
       instances: 'max',
       exec_mode: 'cluster',
       autorestart: true,
+      max_memory_restart: '400M',
+      node_args: '--max-old-space-size=450',
       env: {
         ENV_PATH: __dirname,
       },
