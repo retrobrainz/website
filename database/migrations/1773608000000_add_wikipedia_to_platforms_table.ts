d64d0f1e58afd7171f75d155cf15240fc3ef7781
Guo Yunhe <i@guoyunhe.me>
2026-03-24 21:16:31 +0800

    feat: add position for wikipedia field in platforms table migration

--- database/migrations/1773608000000_add_wikipedia_to_platforms_table.ts
+++ database/migrations/1773608000000_add_wikipedia_to_platforms_table.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.alterTable(this.tableName, (table) => {
-      table.string('wikipedia').nullable();
+      table.string('wikipedia').nullable().after('abbr');
     });
   }
 
